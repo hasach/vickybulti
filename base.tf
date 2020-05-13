d4ecd512e38495bf8ddb74cf2ef69d4697a8dc5c
vsaini44 <vishal@ap2v.com>
2020-05-13 11:45:58 +0530

    Update base.tf

--- base.tf
+++ base.tf
@@ -3,5 +3,13 @@
 provider "aws" {
   region = "ap-south-1"
 }
+resource "aws_instance"  "inst1" {
+  ami = "ami-0470e33cd681b2476"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "gitinstance"
+    }
+}
+
 
 
